0f733fd0d5d0b5419d0b6f7832264ce31e5944e6
Stefan Bayne <stefanbayne@220022522s-MacBook-Pro.local>
2024-07-24 11:42:27 -0400

    feat: trying to get flutter engine running.

--- android/app/src/main/kotlin/com/example/api_request_bloc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/api_request_bloc/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.api_request_bloc
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {}
+class MainActivity: FlutterActivity() {
+    private lateinit var channel: MethodChannel
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger)
+    }
+}
