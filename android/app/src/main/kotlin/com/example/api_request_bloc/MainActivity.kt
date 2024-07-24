f0c8e5637570bcd7255fe4763fed73ade4d68ac0
Stefan Bayne <stefanbayne@220022522s-MacBook-Pro.local>
2024-07-24 12:22:05 -0400

    bugfix: fixing channel in android.

--- android/app/src/main/kotlin/com/example/api_request_bloc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/api_request_bloc/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.api_request_bloc
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import androidx.annotation.NonNull
 
 class MainActivity: FlutterActivity() {
     private lateinit var channel: MethodChannel
@@ -9,6 +11,6 @@ class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
-        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger)
+        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "")
     }
 }
