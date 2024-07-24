d338ea2e5b86128a429376d7ebaa038222516ea0
Stefan Bayne <stefanbayne@220022522s-MacBook-Pro.local>
2024-07-24 15:53:45 -0400

    chore: formatting document.

--- android/app/src/main/kotlin/com/example/api_request_bloc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/api_request_bloc/MainActivity.kt
@@ -1,35 +1,35 @@
 package com.example.api_request_bloc
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import androidx.annotation.NonNull
-import android.content.Intent
-import android.content.IntentFilter
-import android.content.ContextWrapper
-import android.content.Context
-import android.os.BatteryManager
-import android.os.Build.VERSION_CODES
-import android.os.Build.VERSION
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, BATTERY_CHANNEL).setMethodCallHandler {
-          call, result ->
-          if (call.method == "getBatteryLevel") {
-            val batteryLevel = getBatteryLevel()
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, BATTERY_CHANNEL)
+                .setMethodCallHandler { call, result ->
+                    if (call.method == "getBatteryLevel") {
+                        val batteryLevel = getBatteryLevel()
 
-            if (batteryLevel != -1) {
-              result.success(batteryLevel)
-            } else {
-              result.error("UNAVAILABLE", "Battery level not available.", null)
-            }
-          } else {
-            result.notImplemented()
-          }
-        }
+                        if (batteryLevel != -1) {
+                            result.success(batteryLevel)
+                        } else {
+                            result.error("UNAVAILABLE", "Battery level not available.", null)
+                        }
+                    } else {
+                        result.notImplemented()
+                    }
+                }
     }
 
     private fun getBatteryLevel(): Int {
@@ -38,9 +38,13 @@ class MainActivity: FlutterActivity() {
         if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
             val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
             batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-          } else {
-            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        } else {
+            val intent =
+                    ContextWrapper(applicationContext)
+                            .registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel =
+                    intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 /
+                            intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
         }
         return batteryLevel
     }
